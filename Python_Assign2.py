12a4d486f116562bbd01c5ff84402faf7771f81b
hemanth1823 <59700959+hemanth1823@users.noreply.github.com>
2020-08-21 18:59:22 +0530

    Update Python_Assign2.py

--- Python_Assign2.py
+++ Python_Assign2.py
@@ -1,301 +1,74 @@
-{
- "cells": [
-  {
-   "cell_type": "markdown",
-   "metadata": {},
-   "source": [
-    "# method overriding\n",
-    "Having same method in parent class and sub class with different implementation"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "metadata": {
-    "ExecuteTime": {
-     "end_time": "2019-03-27T03:22:46.150809Z",
-     "start_time": "2019-03-27T03:22:46.135188Z"
-    }
-   },
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "hp =  750\n"
-     ]
-    }
-   ],
-   "source": [
-    "class Lamb:\n",
-    "    def car(self):\n",
-    "        print(\"hp = \", 750)\n",
-    "\n",
-    "\n",
-    "class Ferrari(Lamb):\n",
-    "    def car(self):\n",
-    "        print(\"hp = \", 812)\n",
-    "\n",
-    "\n",
-    "l = Lamb()\n",
-    "l.car()"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 2,
-   "metadata": {
-    "ExecuteTime": {
-     "end_time": "2019-03-27T03:22:48.917833Z",
-     "start_time": "2019-03-27T03:22:48.886637Z"
-    }
-   },
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "barking\n",
-      "speaking\n",
-      "barking\n",
-      "None\n"
-     ]
-    }
-   ],
-   "source": [
-    "class animal:\n",
-    "    def speak(self):\n",
-    "        print(\"speaking\")\n",
-    "\n",
-    "\n",
-    "class dog(animal):\n",
-    "    def speak(self):\n",
-    "        print(\"barking\")\n",
-    "\n",
-    "\n",
-    "d1 = dog()\n",
-    "d1.speak()\n",
-    "a1 = animal()\n",
-    "a1.speak()\n",
-    "print(d1.speak())"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 13,
-   "metadata": {
-    "ExecuteTime": {
-     "end_time": "2019-03-27T03:31:21.799724Z",
-     "start_time": "2019-03-27T03:31:21.768477Z"
-    },
-    "scrolled": true
-   },
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "interest rate = 12%\n",
-      "interest rate = 15%\n",
-      "interest rate = 8%\n"
-     ]
-    }
-   ],
-   "source": [
-    "class Canara:\n",
-    "    def interest(self):\n",
-    "        print(\"interest rate = 12%\")\n",
-    "\n",
-    "\n",
-    "class Syn():\n",
-    "    def interest(self):\n",
-    "        print(\"interest rate = 15%\")\n",
-    "\n",
-    "\n",
-    "class SBI():\n",
-    "    def interest(self):\n",
-    "        print(\"interest rate = 8%\")\n",
-    "        \n",
-    "        \n",
-    "c1=Canara()\n",
-    "c1.interest()\n",
-    "\n",
-    "s1=Syn()\n",
-    "s1.interest()\n",
-    "\n",
-    "s2=SBI()\n",
-    "s2.interest()"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 14,
-   "metadata": {
-    "ExecuteTime": {
-     "end_time": "2019-03-27T03:31:24.143315Z",
-     "start_time": "2019-03-27T03:31:24.127694Z"
-    },
-    "scrolled": true
-   },
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "False\n",
-      "True\n"
-     ]
-    }
-   ],
-   "source": [
-    "print(issubclass(Syn,Canara))\n",
-    "print(isinstance(c1,Canara))"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 18,
-   "metadata": {
-    "ExecuteTime": {
-     "end_time": "2019-03-27T03:54:44.573693Z",
-     "start_time": "2019-03-27T03:54:44.558069Z"
-    }
-   },
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "swimming\n",
-      "swimming\n",
-      "swimming backwards\n",
-      "skseleton\n",
-      "swimming\n",
-      "swimming backwards\n",
-      "skseleton\n"
-     ]
-    }
-   ],
-   "source": [
-    "class Fish1:\n",
-    "    def swim(self):\n",
-    "        print(\"swimming\")\n",
-    "\n",
-    "    def swim_backwards(self):\n",
-    "        print(\"swimming backwards\")\n",
-    "\n",
-    "    def skeleton(self):\n",
-    "        print(\"skseleton\")\n",
-    "\n",
-    "\n",
-    "class Fish2(Fish1):\n",
-    "    def swim(self):\n",
-    "        print(\"swimming\")\n",
-    "\n",
-    "    def swim_backwards(self):\n",
-    "        print(\"swimming backwards\")\n",
-    "\n",
-    "    def skeleton(self):\n",
-    "        print(\"skseleton\")\n",
-    "\n",
-    "\n",
-    "f1 = Fish1()\n",
-    "f1.swim()\n",
-    "f2 = Fish2()\n",
-    "\n",
-    "for fish in (f1, f2):\n",
-    "    fish.swim()\n",
-    "    fish.swim_backwards()\n",
-    "    fish.skeleton()"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.2"
-  },
-  "latex_envs": {
-   "LaTeX_envs_menu_present": true,
-   "autoclose": false,
-   "autocomplete": true,
-   "bibliofile": "biblio.bib",
-   "cite_by": "apalike",
-   "current_citInitial": 1,
-   "eqLabelWithNumbers": true,
-   "eqNumInitial": 1,
-   "hotkeys": {
-    "equation": "Ctrl-E",
-    "itemize": "Ctrl-I"
-   },
-   "labels_anchors": false,
-   "latex_user_defs": false,
-   "report_style_numbering": false,
-   "user_envs_cfg": false
-  },
-  "toc": {
-   "base_numbering": 1,
-   "nav_menu": {},
-   "number_sections": true,
-   "sideBar": true,
-   "skip_h1_title": false,
-   "title_cell": "Table of Contents",
-   "title_sidebar": "Contents",
-   "toc_cell": false,
-   "toc_position": {},
-   "toc_section_display": true,
-   "toc_window_display": false
-  },
-  "varInspector": {
-   "cols": {
-    "lenName": 16,
-    "lenType": 16,
-    "lenVar": 40
-   },
-   "kernels_config": {
-    "python": {
-     "delete_cmd_postfix": "",
-     "delete_cmd_prefix": "del ",
-     "library": "var_list.py",
-     "varRefreshCmd": "print(var_dic_list())"
-    },
-    "r": {
-     "delete_cmd_postfix": ") ",
-     "delete_cmd_prefix": "rm(",
-     "library": "var_list.r",
-     "varRefreshCmd": "cat(var_dic_list()) "
-    }
-   },
-   "types_to_exclude": [
-    "module",
-    "function",
-    "builtin_function_or_method",
-    "instance",
-    "_Feature"
-   ],
-   "window_display": false
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+# Create an empty list using square brackets.
+numbers = []
+print(numbers) # Output: []
+
+# Create an empty list using list().
+numbers = list()
+print(numbers) # Output: []
+
+# Create a list of numbers.
+numbers = [1, 2, 3]
+print(numbers) # Output: [1, 2, 3]
+
+# Create a list of numbers in a range.
+numbers = list(range(1, 4))
+print(numbers) # Output: [1, 2, 3]
+
+# Create a list of tuples.
+tuples_list = [(1, 2), (2, 4), (3, 6)]
+print(tuples_list) # Output: [(1, 2), (2, 4), (3, 6)]
+
+# Create a list of lists.
+list_of_lists = [[1, 2], [2, 4], [3, 6]]
+print(list_of_lists) # Output: [[1, 2], [2, 4], [3, 6]]
+
+# Create a list with items of different data types.
+random_list = [1, "hey", [1, 2]]
+print(random_list) # Output: [1, "hey", [1, 2]]
+
+# Get length of list by using len() method.
+numbers = [5, 8, 8]
+print(len(numbers)) # Output: 3
+
+# Access elements of a list by indexing.
+str_list = ["hey", "there!", "how", "are", "you?"]
+print(str_list[0]) # Output: "hey"
+print(str_list[len(str_list) - 1]) # Output: "you?"
+print(str_list[-1]) # Output: "you?"
+
+# Slicing a list.
+str_list = ["hey", "there!", "how", "are", "you?"]
+print(str_list[2:]) # Output: ["how", "are", "you?"]
+print(str_list[:2]) # Output: ["hey", "there!"]
+print(str_list[-3:]) # Output: ["how", "are", "you?"]
+print(str_list[:-3]) # Output: ["hey", "there!"]
+print(str_list[1:4]) # Output: ["there!", "how", "are"]
+# Get a copy of list by slicing.
+print(str_list[:]) # Output: ["hey", "there!", "how", "are", "you?"]
+
+# Append to a list.
+numbers = [1, 2]
+print(numbers) # Output: [1, 2]
+numbers.append(3)
+print(numbers) # Output: [1, 2, 3]
+
+# Concatenate lists.
+numbers = [1, 2]
+strings = ["Hey", "there"]
+print(numbers + strings) # Output: [1, 2, "Hey", "there"]
+
+# Mutate a list, that is, change its contents.
+numbers = [1, 2, 3]
+numbers[0] = 100
+print(numbers) # Output: [100, 2, 3]
+numbers[0:2] = [300, 400]
+print(numbers) # Output: [300, 400, 3]
+numbers[1:3] = []
+print(numbers) # Output: [300]
+numbers[:] = []
+print(numbers) # Output: []
+
+# Insert item to a list.
+greeting = ["how", "you?"]
+greeting.insert(1, "are")
+print(greeting) # Output: ["how", "are", "you?"]
